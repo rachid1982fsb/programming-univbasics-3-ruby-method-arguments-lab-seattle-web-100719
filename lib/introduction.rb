18b916a0bbd6365dcb0db873ab9a5ff39e5b86db
Rachid Rakaa <rachid1982fsb@gmail.com>
2019-09-27 23:38:07 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,9 +7,11 @@
  end
  
 def introduction_with_language(name,language)
+   "Hi, my name is #{name} and I am learning to program in #{language}."
+ end
 
 puts introduction("Rachid")
-
+puts introduction_with_language("Zineb","Arabic")
 
 
 
